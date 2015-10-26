38b44f20591759efb24c162e6c1807ffe2b362cb
Rennie Bevineau <rsbevineau@gmail.com>
2015-10-26 10:22:24 -0600

    add authorization key

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 var rhymeApp = angular.module('rhymeApp', ['ngResource']);
 
 rhymeApp.controller('rhymeView', function ($scope, $resource) {
-		$scope.rhymeApi = $resource("http://store.apicultur.com/api/rima/1.0.0/flor/true/0/200/true", {callBack: "JSON_CALLBACK"}, { get: {method: "JSONP"}});
-		$scope.rhymeResult = $scope.rhymeApi.get();
 
-		console.log($scope.rhymeResult);
+$scope.rhymeApi = $resource("http://store.apicultur.com/api/rima/1.0.0/flor/true/0/200/true",
+{ get: { method: 'GET', headers : { 'auth-key' : '49vlBAfa41zQclqtq4DXIxzzU3oa' }}});
+
+$scope.rhymeApi.get({},function (result) {
+	console.log(result);
+});
+
 });
